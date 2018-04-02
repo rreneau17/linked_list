7b9f821f0e501ae02f24827100a11ac3a86bc9ad
Rich Reneau <rich.reneau@gmail.com>
2018-04-02 19:19:53 -0400

    added lookup, remove, and more functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,55 +1,115 @@
-function Node(data){
-    this.data=data;
-    this.next=null;
-}
-function Solution(){
-    this.insert=function(head,data){
-        var node = new Node(data);
-        if (head == null){
-            head = node
-        }
-        else {
-            var temp = head;
-            // console.log(temp.next);
-            while (temp.next != null)
-                temp = temp.next
-                temp.next = node;
-        }
-        console.log(JSON.stringify(head));
+function LinkedList() {
+    var length = 0;
+    var head = null;
+
+    var Node = function(data) {
+        this.data = data;
+        this.next = null;
+    };
+
+    this.size = function() {
+        return length;
+    };
+
+    this.head = function() {
         return head;
-    };
-    this.display=function(head){
-        var start=head;
-            while(start){
-                //console.log(start.data);
-                // console.log('next :', start.next);
-                start=start.next;
+    }
+
+    this.add = function(data) {
+        var node = new Node(data);
+        if(head === null) {
+            head = node;
+        } else {
+            currentNode = head;
+
+            while(currentNode.next) {
+                currentNode = currentNode.next;
             }
+
+            currentNode.next = node;    
+        }
+        length++;
     };
+
+    this.remove = function(data) {
+        var currentNode = head;
+        var previousNode;
+        if (currentNode.data === data) {
+            head = currentNode.next;
+        } else {
+            while(currentNode.data !== data) {
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+            }
+            previousNode.next = currentNode.next;
+        }
+        length --;
+    };
+
+    this.isEmpty = function(data) {
+        return length === 0;
+    }
+
+    this.indexOf = function(data) {
+        var currentNode = head;
+        var index = -1;
+
+        while(currentNode) {
+            index++;
+            if(currentNode.data === data) {
+                return index;
+            }
+            currentNode = currentNode.next;
+        }
+        return -1;
+    };
+
+    this.elementAt = function(index) {
+        var currentNode = head;
+        var count = 0;
+        while (count < index) {
+            count ++;
+            currentNode = currentNode.next;
+        }
+        return currentNode.data;
+    };
+    
+    this.addAt = function(index, data) {
+        var node = new Node(element);
+
+        var currentNode = head;
+        var previousNode;
+        var currentIndex = 0;
+
+        if(index > length) {
+            return false;
+        }
+
+        if(index === 0) {
+            node.next = currentNode;
+            head = node;
+        } else {
+            while(currentIndex < index) {
+                currentIndex++;
+                previousNode = currentNode;
+                currentNode = currentNode.next;
+            }
+            node.next = currentNode;
+            previousNode.next = node;
+        }
+        length++;
+    }
 }
 
-
-
-function main(){
-    var T = 4;
-    var head=null;
-    var mylist=new Solution();
-    linkedArray = [2, 3, 4, 1];
-    for(i=0;i<T;i++){
-        var data=linkedArray[i];
-        head=mylist.insert(head,data);
-    }
-    mylist.display(head);
-}	
-
-function checking() {
-    var testArr = [1,2,3];
-    i = 0;
-    while (testArr[i]) {
-        console.log(testArr[i]);
-        i++;
-    }
+var conga = new LinkedList();
+conga.add('Kitten');
+conga.add('Puppy');
+conga.add('Dog');
+conga.add('Cat');
+conga.add('Fish');
+console.log(conga.size());
+for (var i=0; i<conga.size(); i++ ) {
+    console.log(conga.elementAt(i));
 }
+console.log(conga);
 
-main();
-checking();
